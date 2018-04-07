025bc1574e3468d79a088a6a0192932ec96d1bcf
Lalaithion <izaak.weiss@gmail.com>
2018-04-07 17:02:32 -0600

    logchain can now verify itself

--- logchain/src/lib.rs
+++ logchain/src/lib.rs
@@ -3,12 +3,13 @@ extern crate byteorder;
 extern crate bigint;
 
 use std::io::prelude::*;
-use std::io::Cursor;
 use std::fs::OpenOptions;
+use std::fs::File;
+use std::io::BufReader;
 
 use crypto::{digest::Digest, sha2::Sha256};
 
-use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+use byteorder::{BigEndian, WriteBytesExt};
 
 use bigint::{U256};
 
@@ -21,16 +22,17 @@ macro_rules! assert_ok(
     };
 );
 
-
 pub fn valid(bytes: &[u8; 32]) -> bool {
-    let mut rdr = Cursor::new(bytes.to_vec());
-    let num = rdr.read_u32::<BigEndian>().unwrap();
-    true
+    let difficulty = U256::max_value() / (U256::from(100u32));
+
+    let hashnum = U256::from(bytes.iter().as_slice());
+
+    hashnum < difficulty
 }
 
-pub fn hash_u32(num : u32) -> [u8; 32] {
+pub fn hash_u32(num : u32) -> [u8; 4] {
     let mut wtr = vec![];
-    let mut result = [0; 32];
+    let mut result = [0; 4];
 
     wtr.write_u32::<BigEndian>(num).unwrap();
     result.clone_from_slice(&wtr);
@@ -39,13 +41,31 @@ pub fn hash_u32(num : u32) -> [u8; 32] {
 }
 
 pub fn to_hex_string(bytes: &[u8]) -> String {
-  let strs: Vec<String> = bytes.iter()
-                               .map(|b| format!("{:02X}", b))
-                               .collect();
-  strs.join("")
+    let strs: Vec<String> = bytes
+        .iter()
+        .map(|b| format!("{:02X}", b))
+        .collect();
+
+    strs.join("")
 }
 
 
+pub fn from_hex_string(s: &str) -> [u8; 32] {
+    let mut res : [u8; 32] = [0; 32];
+    let mut i : usize = 0;
+
+    for b in s.as_bytes().chunks(2) {
+        let b0 = (b[0] as char).to_digit(16).unwrap() as u8;
+        let b1 = (b[1] as char).to_digit(16).unwrap() as u8;
+        res[i] = (b0 * 16) + b1;
+        i += 1
+    }
+
+    res
+}
+
+
+#[derive(Debug)]
 struct LogEntry {
     data: String
 }
@@ -61,7 +81,7 @@ fn entry_hash(e : &LogEntry) -> [u8; 32] {
     result
 }
 
-
+#[derive(Debug)]
 struct LogBlock {
     entry: LogEntry,
     nonce: u32,
@@ -77,7 +97,6 @@ fn mk_log_block(msg : String, prev_hash: &[u8; 32]) -> LogBlock {
     let mut curr = LogBlock{entry: entry, nonce: 0, previous_hash: prev};
 
     while !valid(&block_hash(&curr)) {
-        println!("{}", curr.nonce);
         curr.nonce += 1;
     }
 
@@ -100,9 +119,10 @@ fn format_block(b: &LogBlock) -> String {
     strs.push(curr);
     strs.push(":".to_string());
     strs.push(data.to_owned());
-    strs.push("\n".to_string());
 
-    strs.join(" ")
+    let mut res = strs.join(" ");
+    res.push('\n');
+    res
 }
 
 
@@ -113,7 +133,7 @@ fn block_hash(e : &LogBlock) -> [u8; 32] {
     let mut hasher = Sha256::new();
     hasher.input(&e.previous_hash);
     hasher.input(&subhash);
-    //hasher.input(&hash_u32(e.nonce));
+    hasher.input(&hash_u32(e.nonce));
     hasher.result(&mut result);
 
     result
@@ -168,6 +188,36 @@ impl Logger for LogFile {
 }
 
 
+pub fn validate(filename: &str) -> Result<bool, std::io::Error> {
+    let f = BufReader::new(File::open(filename)?);
+
+    for line in f.lines() {
+        println!("{:?}", line);
+        let okline = line?;
+        let mut parts = okline.split(" : ");
+        let hashstr = parts.next().unwrap();
+        let prevstr = parts.next().unwrap();
+        let noncestr = parts.next().unwrap();
+        let datahash = parts.next().unwrap();
+        let data : Vec<&str> = parts.collect();
+        let data = data.join(" : ");
+
+        let block = LogBlock {
+            entry: LogEntry {data : data},
+            nonce: noncestr.parse::<u32>().unwrap(),
+            previous_hash: from_hex_string(prevstr)
+        };
+
+        println!("{:?}", block);
+
+        let hash = block_hash(&block);
+        assert_eq!(hashstr, to_hex_string(&hash));
+    }
+
+    Ok(true)
+}
+
+
 #[cfg(test)]
 mod tests {
     use *;
@@ -178,5 +228,28 @@ mod tests {
         assert_ok!(logger.log("Hello world!"));
         assert_ok!(logger.log("This is a test."));
         assert_ok!(logger.log("Alright!"));
+    }
+
+    #[test]
+    fn hex_strs() {
+        let arr = [1,2,3,4,5,6,7,8,
+            9,10,11,12,13,14,15,16,
+            17,18,19,20,21,22,23,24,
+            25,26,27,28,29,30,31,32];
+
+        assert_eq!(arr, from_hex_string(&to_hex_string(&arr)));
+    }
+
+    #[test]
+    fn validate_log() {
+        let mut logger = LogFile::new([0;32], "write_log_test.logc");
+        assert_ok!(logger.log("Hello world!"));
+        assert_ok!(logger.log("This is a test."));
+        assert_ok!(logger.log("Alright!"));
+
+        let res = validate("write_log_test.logc");
+        assert_ok!(res);
+        let b = res.unwrap();
+        assert!(b);
     }
 }
