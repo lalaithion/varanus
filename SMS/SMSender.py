94c02d2fc02d8ed4c66e28516716ffe8429e38d2
aowsenek <anya.owsenek@gmail.com>
2018-04-07 13:23:48 -0600

    removed SMS folder

--- SMS/SMSender.py
+++
@@ -1,9 +0,0 @@
-from twilio.rest import Client
-from authTokens import account_sid, auth_token
-client = Client(account_sid, auth_token)
-client.messages.create(
-  to="+15716062372",
-  from_="+16027867186",
-  body="Test Post Please Ignore"
-  )
-
