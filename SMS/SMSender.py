bb84f479006bed1484ad145d1b29b2ee4ec58849
aowsenek <anya.owsenek@gmail.com>
2018-04-07 12:48:00 -0600

    Send SMS

---
+++ SMS/SMSender.py
@@ -0,0 +1,9 @@
+from twilio.rest import Client
+from ../../authTokens import account_sid, auth_token
+client = Client(account_sid, auth_token)
+client.messages.create(
+  to="+15716062372",
+  from_="+16027867186",
+  body="Test Post Please Ignore"
+  )
+
